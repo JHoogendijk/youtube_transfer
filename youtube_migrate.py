13a16a19bfbd008387f2250b2c22493c1b4f5713
Jesca Hoogendijk <jesca.hoogendijk@gmail.com>
2020-01-26 12:25:56 +0100

    Update youtube_migrate.py

--- youtube_migrate.py
+++ youtube_migrate.py
@@ -4,7 +4,7 @@ YouTube account with Python and Selenium.
 
 Tested with:
  - selenium 3.0
- - firefox 49.0
+ - chrome 79.0
  - python 3.5
 
  1. Install selenium from pypi:
@@ -12,14 +12,26 @@ Tested with:
 
  2. Go to the down of page https://www.youtube.com/subscription_manager
     and download your current subscriptions feed.
-    Save file as subscription_manager.xml.
+    Save file as subscription_manager.xml in current folder or update the runtime variable below
+    
+ 3. Use the youtube api to fetch a list of likes with the attributes snippet and contentDetails.
+    Save as likes.json in current folder or update the runtime variable below
+ 
+ 4. Create a new profile in chrome and login with your google account
+ 
+ 5. Copy the profile folder (found using chrome://version) and rename the folder to Default. 
+    Place the folder in an empty parent folder and paste the parent folder's path in the variable below
 
- 4. Run script, enter your credentials and go to drink coffee.
+ 6. Run script and go to drink coffee.
     It will take some time.
-
-Note YouTube will temporary block you if you have more that 80 subscriptions.
-Just restart the script in a few hours.
 """
+
+# Variables likely to need updating after youtube design changes
+like_button_with_pressed_attribute = 'ytd-toggle-button-renderer.style-scope.ytd-menu-renderer.force-icon-button button' #This button is not pressable and only contains an attribute that shows the video was already liked
+like_button_to_press = 'ytd-toggle-button-renderer.style-scope.ytd-menu-renderer.force-icon-button.style-text'
+subscribe_button = '#subscribe-button'
+youtube_video_url_base = 'https://www.youtube.com/watch?v='
+youtube_channel_url_base = 'https://www.youtube.com/channel/'
 
 from collections import namedtuple
 from selenium import webdriver
@@ -29,12 +41,15 @@ import json
 import time
 import re
 
+# Runtime variables
+likes_json_locaion = "./likes.json"
+subscriptions_location = "./subscription_manager.xml"
 
 def main():
     options = Options()
     options.add_argument("user-data-dir=C:\\Users\\Jesca\\Desktop\\youtube transfer\\profile")
     driver = webdriver.Chrome(options=options)
-    #transfer_subscribtions(driver)
+    transfer_subscribtions(driver)
     transfer_likes(driver)
     driver.close()
     
@@ -44,17 +59,17 @@ def transfer_likes(driver):
         
         
 def video_is_liked(driver):
-    result = driver.find_element_by_css_selector('ytd-toggle-button-renderer.style-scope.ytd-menu-renderer.force-icon-button button').get_attribute("aria-pressed")
+    result = driver.find_element_by_css_selector(like_button_with_pressed_attribute).get_attribute("aria-pressed")
     return result=="true"
         
 
 def like(driver, video):
     try:
-        channel_url = 'https://www.youtube.com/watch?v=' + video["id"]
+        channel_url = youtube_video_url_base + video["id"]
         driver.get(channel_url)
         time.sleep(1)
 
-        button = driver.find_element_by_css_selector('ytd-toggle-button-renderer.style-scope.ytd-menu-renderer.force-icon-button.style-text')
+        button = driver.find_element_by_css_selector(like_button_to_press)
         is_liked = video_is_liked(driver)
             
         if not is_liked:
@@ -66,7 +81,7 @@ def like(driver, video):
     time.sleep(1)
     
 def load_likes():
-    input_file = open ('likes.json')
+    input_file = open (likes_json_locaion)
     json_array = json.load(input_file, encoding="utf8")
     videos_to_like = list()
     for like in reversed(json_array):
@@ -83,7 +98,7 @@ def transfer_subscribtions(driver):
      
 
 def load_subcribtions():
-    xmldoc = minidom.parse('subscription_manager.xml')
+    xmldoc = minidom.parse(subscriptions_location)
     itemlist = xmldoc.getElementsByTagName('outline')
     channel_id_regexp = re.compile('channel_id=(.*)$')
     Channel = namedtuple('Channel', ['id', 'title'])
@@ -103,11 +118,11 @@ def load_subcribtions():
 
 def subscribe(driver, channel):
     try:
-        channel_url = 'https://www.youtube.com/channel/' + channel.id
+        channel_url = youtube_channel_url_base + channel.id
         driver.get(channel_url)
         time.sleep(1)
 
-        button = driver.find_element_by_css_selector('#subscribe-button')
+        button = driver.find_element_by_css_selector(subscribe_button)
         is_subscribed = button.get_attribute('data-is-subscribed')
 
         if not is_subscribed:
