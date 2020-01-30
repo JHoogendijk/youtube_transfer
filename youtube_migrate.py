139789a81669f5e4b7fb4615ae7b85c6e8e2935c
Jesca Hoogendijk <jesca.hoogendijk@gmail.com>
2020-01-30 22:36:40 +0100

    Update youtube_migrate.py

--- youtube_migrate.py
+++ youtube_migrate.py
@@ -7,6 +7,7 @@ Tested with:
  - python 3.5
  1. Install selenium from pypi:
     $ pip install selenium
+    
  2. Go to the down of page https://www.youtube.com/subscription_manager
     and download your current subscriptions feed.
     Save file as subscription_manager.xml in current folder or update the runtime variable below
@@ -18,7 +19,10 @@ Tested with:
  
  5. Copy the profile folder (found using chrome://version) and rename the folder to Default. 
     Place the folder in an empty parent folder and paste the parent folder's path in the variable below
- 6. Run script and go to drink coffee.
+    
+ 6. Close all instances of chrome
+    
+ 7. Run script and go to drink coffee.
     It will take some time.
 """
 
@@ -38,10 +42,14 @@ import json
 import time
 import re
 import argparse
+import pickle
+import os.path
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--likes_location', '-ll', help='file containing the likes', type=str, default='./likes.json', dest='likes_location')
 parser.add_argument('--subscriptions_location', '-sl', help='file containing the subscriptions', type=str, default='./subscription_manager.xml', dest='subscriptions_location')
+parser.add_argument('--likes_done_location', '-ldl', help='file containing the likes already done', type=str, default='./likes_done', dest='likes_done_location')
+parser.add_argument('--subscriptions_done_location', '-sdl', help='file containing the subscriptions already done', type=str, default='./subscriptions_done', dest='subscriptions_done_location')
 parser.add_argument("--transfer_subscriptions", '-ts', type=bool, nargs='?',
                         const=True, default=False,
                         help="Add this argument to transfer subscriptions",
@@ -57,28 +65,48 @@ likes_json_locaion = args.likes_location
 subscriptions_location = args.subscriptions_location
 do_subscriptions = args.transfer_subscriptions
 do_likes = args.transfer_likes
+likes_done_path = args.likes_done_location
+subscriptions_done_path = args.subscriptions_done_location
 
 def main():
     options = Options()
     options.add_argument("user-data-dir=C:\\Users\\Jesca\\Desktop\\youtube transfer\\profile")
     driver = webdriver.Chrome(options=options)
     if do_subscriptions:
-        transfer_subscribtions(driver)
+        transfer_subscriptions(driver)
     if do_likes:
         transfer_likes(driver)
     driver.close()
     
 def transfer_likes(driver):
-    for video_to_like in load_likes():
-        like(driver, video_to_like)
+    maybe_create_likes_done()
+    likes_done = load_likes_done()
+    videos_to_like = load_likes(likes_done)
+    i = 0
+    n = len(videos_to_like)
+    for video_to_like in videos_to_like:
+        i += 1
+        likes_done = like(driver, video_to_like, likes_done, i, n)
         
+def maybe_create_likes_done():
+    exists = os.path.exists(likes_done_path)
+    if not exists:
+        with open(likes_done_path, 'wb') as fp:
+            pickle.dump([], fp)
+        
+def load_likes_done():
+    with open(likes_done_path, 'rb') as fp:
+        return pickle.load(fp)
+        
+def save_like_done(likes_done):
+    with open(likes_done_path, 'wb') as fp:
+        pickle.dump(likes_done, fp)
         
 def video_is_liked(driver):
     result = driver.find_element_by_css_selector(like_button_with_pressed_attribute).get_attribute("aria-pressed")
     return result=="true"
-        
 
-def like(driver, video):
+def like(driver, video, likes_done, i, n):
     try:
         channel_url = youtube_video_url_base + video["id"]
         driver.get(channel_url)
@@ -89,13 +117,16 @@ def like(driver, video):
             
         if not is_liked:
             button.click()
-
-        print('{:.<50}{}'.format(video["title"], 'skip' if is_liked else 'done'))
+        
+        likes_done.append(video["id"])
+        save_like_done(likes_done)
+        print('{:.<70}{}'.format(video["title"][:67], 'skip' + '(' + str(i) + '/'+ str(n) + ')' if is_liked else 'done' + '(' + str(i) + '/'+ str(n) + ')'))
     except:
-        print('{:.<50}{}'.format(video["title"], "Whoops! Something went wrong! Moving on!"))
+        print('{:.<70}{}'.format(video["title"][:67], "Whoops! Something went wrong! Moving on!" + '(' + str(i) + '/'+ str(n) + ')'))
     time.sleep(1)
+    return likes_done
     
-def load_likes():
+def load_likes(likes_done):
     input_file = open (likes_json_locaion)
     json_array = json.load(input_file, encoding="utf8")
     videos_to_like = list()
@@ -103,16 +134,23 @@ def load_likes():
         video_to_like = dict()
         video_to_like["title"] = like["snippet"]["title"]
         video_to_like["id"] = like["contentDetails"]["videoId"]
-        videos_to_like.append(video_to_like)
+        if (not video_to_like["id"] in likes_done) and (not video_to_like["title"] == "Private video"):
+            videos_to_like.append(video_to_like)
     return videos_to_like
     
     
-def transfer_subscribtions(driver):
-    for channel in load_subcribtions():
-        subscribe(driver, channel)
+def transfer_subscriptions(driver):
+    maybe_create_likes_done()
+    subscriptions_done = load_subscriptions_done()
+    subscriptions = load_subcriptions(subscriptions_done)
+    n = len(subscriptions)
+    i = 0
+    for channel in subscriptions:
+        i += 1
+        subscriptions_done = subscribe(driver, channel, subscriptions_done, i, n)
      
 
-def load_subcribtions():
+def load_subcriptions():
     xmldoc = minidom.parse(subscriptions_location)
     itemlist = xmldoc.getElementsByTagName('outline')
     channel_id_regexp = re.compile('channel_id=(.*)$')
@@ -129,9 +167,22 @@ def load_subcribtions():
             pass
 
     return subscriptions
+        
+def maybe_create_subscriptions_done():
+    exists = os.path.exists(subscriptions_done_path)
+    if not exists:
+        with open(subscriptions_done_path, 'wb') as fp:
+            pickle.dump([], fp)
+        
+def load_subscriptions_done():
+    with open(subscriptions_done_path, 'rb') as fp:
+        return pickle.load(fp)
+        
+def save_subscriptions_done(subscriptions_done):
+    with open(subscriptions_done_path, 'wb') as fp:
+        pickle.dump(subscriptions_done, fp)
 
-
-def subscribe(driver, channel):
+def subscribe(driver, channel, subscriptions_done, i, n):
     try:
         channel_url = youtube_channel_url_base + channel.id
         driver.get(channel_url)
@@ -143,10 +194,13 @@ def subscribe(driver, channel):
         if not is_subscribed:
             button.click()
 
-        print('{:.<50}{}'.format(channel.title, 'skip' if is_subscribed else 'done'))
+        if (not channel.id in likes_done):
+            subscriptions_done.append(video_to_like)
+        print('{:.<70}{}'.format(channel.title[:67], 'skip' + '(' + str(i) + '/'+ str(n) + ')' if is_subscribed else 'done' + '(' + str(i) + '/'+ str(n) + ')'))
     except:
-        print('{:.<50}{}'.format(channel.title, "Whoops! Something went wrong! Moving on!"))
+        print('{:.<70}{}'.format(channel.title[:67], "Whoops! Something went wrong! Moving on!" + '(' + str(i) + '/'+ str(n) + ')'))
     time.sleep(1)
+    return subscriptions_done
 
 
 if __name__ == '__main__':
